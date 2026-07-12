e8655be90336d2e933f2fca360828290d61fa50e
cymweng <milesweng12@gmail.com>
2026-07-11 20:21:33 -0400

    Initial custom dashboard setup

---
+++ dashboard/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#0d0d0d",
+        width: "100vw",
+        height: "100vh",
+        color: "white",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "48px",
+        fontFamily: "Arial",
+      }}
+    >
+      Segway Dashboard
+    </div>
+  );
+}
+
+export default App;
